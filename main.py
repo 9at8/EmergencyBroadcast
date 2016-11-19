0fbb7feed94ce065aa776da59a8f1790273fe6cb
Aditya Thakral <aditya.1998thakral@gmail.com>
2016-11-19 01:58:13 -0500

    Test for audio

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from subprocess import call
 
 for freq in range(880, 1070):
-    call('sudo timeout 10 ~/PiFmRds/src/./pi_fm_rds -freq ' + str(freq/10), shell=True)
+    call('sudo timeout 10 ~/PiFmRds/src/./pi_fm_rds -audio ~/EmergencyBroadcast/lookdave.wav -freq ' + str(freq / 10)
+    shell = True)
